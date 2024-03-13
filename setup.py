88104c24ee68e80abdae82303aded02ef25a5379
Sachin kumar <sachinkr1416@gmail.com>
2024-03-13 10:13:51 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+from typing import List
+
+Hypen_e_dot='-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n"," ")for req in requirements]
+        
+        
+        if Hypen_e_dot in requirements:
+            requirements.remove(Hypen_e_dot)
+        
+        return requirements
+
+
+
+setup(
+    name="DiamondPricePrediction",
+    author="SachinKr",
+    author_email="sachinkr1416@gmail.com",
+    version='0.0.1',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+     
+)
